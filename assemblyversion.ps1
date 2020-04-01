e4e3b7aa141ab3d54130b59b3d6a5c56a5d9ec5a
Danish Naglekar <36135520+Power-Maverick@users.noreply.github.com>
2020-04-01 17:53:37 -0400

    Update assemblyversion.ps1

--- assemblyversion.ps1
+++ assemblyversion.ps1
@@ -31,7 +31,6 @@ function Update-AssemblyInfoVersionFile
     $major = $productVersion.Split('.')[0]
     $minor = $buildYear
     $patch = $buildMonth
-    $buildIncrementalNumber = $buildDate
 
     $assemblyVersion = "$major.$minor.$patch.$buildIncrementalNumber"
     $assemblyFileVersion = "$major.$minor.$patch.$buildIncrementalNumber"
